dd066c09dd17a54ce25e02f2a0697e4efd8717dc
zzf <3556526173@qq.com>
2023-06-27 21:16:40 +0800

    提交

--- zhaozongfu/6.25考试(赵宗福)/js/壁纸管理.js
+++ zhaozongfu/6.25考试(赵宗福)/js/壁纸管理.js
@@ -54,60 +54,63 @@ $(document).ready(function () {
 
         $('body').toggleClass('fff12');
     })
+    $("#span").on("click", function () {
+
+        $('body').toggleClass('fff');
+    })
+    $(".div1").on("click", function () {
+
+        $('body').toggleClass('div1');
+    })
+    $(".div2").on("click", function () {
+
+        $('body').toggleClass('div2');
+    })
+    $(".div3").on("click", function () {
+
+        $('body').toggleClass('div3');
+    })
+    $(".div4").on("click", function () {
+
+        $('body').toggleClass('div4');
+    })
+    $(".div5").on("click", function () {
+        $('body').toggleClass('div5');
+    })
+    $(".div6").on("click", function () {
+
+        $('body').toggleClass('div6');
+    })
+    $(".div7").on("click", function () {
+
+        $('body').toggleClass('div7');
+    })
+    $(".div8").on("click", function () {
+
+        $('body').toggleClass('div8');
+    })
+    $(".div9").on("click", function () {
+        $('body').toggleClass('div9');
+    })
+    $(".div10").on("click", function () {
+
+        $('body').toggleClass('div10');
+    })
+    $(".div11").on("click", function () {
+
+        $('body').toggleClass('div11');
+    })
+    $(".div11").on("click", function () {
+
+        $('body').toggleClass('div12');
+    })
 });
-/* const colorli = [
-    {
-        color: "red"
-    },
-    {
-        color1: "pink"
-    },
-    {
-        color2: "blue"
-    },
-    {
-        color3: "yellow"
-    },
-    {
-        color4: "green"
-    },
-    {
-        color5: "aqua"
-    },
-    {
-        color6: "gray"
-    },
-    {
-        color7: "yellowgreen"
-    },
-    {
-        color8: "block"
-    },
-    {
-        color9: "#fff"
-    }
-];
-$(function () {
-    styleCss();
-});
-function styleCss() {
-    let html = "";
-    colorli.forEach((item, index) => {
-        html +=
-            `
-        <div style="background-color:${item.color};"></div>
-        <div style="background-color:${item.color1};"></div>
-        <div style="background-color:${item.color2};"></div>
-        <div style="background-color:${item.color3};"></div>
-        <div style="background-color:${item.color4};"></div>
-        <div style="background-color:${item.color5};"></div>
-        <div style="background-color:${item.color6};"></div>
-        <div style="background-color:${item.color7};"></div>
-        <div style="background-color:${item.color8};"></div>
-        <div style="background-color:${item.color9};"></div>
-        <div style="background-color:${item.color10};"></div>
-        `
-    });
-    $("#box3").html(html);
-}
- */
+  $(".icon1").click(function(){
+    $(".lloo").fadeIn(3000);
+    $("#box").css("display", "none");
+    
+  })
+  $(".lloo").click(function(){
+    $("#box").fadeIn(3000);
+    $(".lloo").css("display", "none");
+  })
