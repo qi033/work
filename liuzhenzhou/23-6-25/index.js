4af897a7305841b915384fc59765a05946c5ca48
铁蛋 <2076865717@qq.com>
2023-06-28 19:50:15 +0800

    tatus

--- liuzhenzhou/23-6-25/index.js
+++ liuzhenzhou/23-6-25/index.js
@@ -195,52 +195,52 @@ $("#bos a").on("click", function () {
 $("#show").on("click", function () {
   $("#bos").css("display", "block");
 });
-// //使 DIV 元素拖动：
-// dragElement(document.getElementById("bos"));
+//使 DIV 元素拖动：
+dragElement(document.getElementById("bos"));
 
-// function dragElement(elmnt) {
-//   var pos1 = 0,
-//     pos2 = 0,
-//     pos3 = 0,
-//     pos4 = 0;
-//   if (document.getElementById(elmnt.id + "header")) {
-//     /* 如果存在，header 就是你移动 DIV 的地方：*/
-//     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-//   } else {
-//     /* 否则，将 DIV 从 DIV 内的任何位置移动：*/
-//     elmnt.onmousedown = dragMouseDown;
-//   }
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* 如果存在，header 就是你移动 DIV 的地方：*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* 否则，将 DIV 从 DIV 内的任何位置移动：*/
+    elmnt.onmousedown = dragMouseDown;
+  }
 
-//   function dragMouseDown(e) {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // 在启动时获取鼠标光标位置：
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     document.onmouseup = closeDragElement;
-//     // 当光标移动时调用一个函数：
-//     document.onmousemove = elementDrag;
-//   }
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // 在启动时获取鼠标光标位置：
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // 当光标移动时调用一个函数：
+    document.onmousemove = elementDrag;
+  }
 
-//   function elementDrag(e) {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // 计算新的光标位置：
-//     pos1 = pos3 - e.clientX;
-//     pos2 = pos4 - e.clientY;
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     // 设置元素的新位置：
-//     elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-//     elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-//   }
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // 计算新的光标位置：
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // 设置元素的新位置：
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
 
-//   function closeDragElement() {
-//     /* 释放鼠标按钮时停止移动：*/
-//     document.onmouseup = null;
-//     document.onmousemove = null;
-//   }
-// }
+  function closeDragElement() {
+    /* 释放鼠标按钮时停止移动：*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 layui.use(function () {
   var upload = layui.upload;
   var layer = layui.layer;
