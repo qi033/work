113da7bf74aa843829ca8048182f5c17badf1e3d
铁蛋 <2076865717@qq.com>
2023-06-30 16:02:48 +0800

    one

---
+++ liuzhenzhou/23-6-30/script/data.js
@@ -0,0 +1,150 @@
+const students = [
+  {
+    name: "刘佳佳",
+    result: [
+      {
+        dataList: "6.2(星期五)",
+        subject: [
+          { name: "笔试", score2: 72.5 },
+          { name: "机试", score1: 95 },
+          { name: "平均分", score: 83.75 },
+        ],
+      },
+      {
+        dataList: "6.9(星期五)",
+        subject: [
+          { name: "笔试", score2: 47.5 },
+          { name: "机试", score1: 90 },
+          { name: "平均分", score: 68.75 },
+        ],
+      },
+      {
+        dataList: "6.16(星期五)",
+        subject: [
+          { name: "笔试", score2: 50 },
+          { name: "机试", score1: 70 },
+          { name: "平均分", score: 60 },
+        ],
+      },
+      {
+        dataList: "6.21(星期五)",
+        subject: [
+          { name: "笔试", score2: 53 },
+          { name: "机试", score1: 50 },
+          { name: "平均分", score: 51.5 },
+        ],
+      },
+    ],
+  },
+  {
+    name: "赵宗福",
+    result: [
+      {
+        dataList: "6.2(星期五)",
+        subject: [
+          { name: "笔试", score2: 60 },
+          { name: "机试", score1: 95 },
+          { name: "平均分", score: 77.5 },
+        ],
+      },
+      {
+        dataList: "6.9(星期五)",
+        subject: [
+          { name: "笔试", score2: 53 },
+          { name: "机试", score1: 90 },
+          { name: "平均分", score: 71.5 },
+        ],
+      },
+      {
+        dataList: "6.16(星期五)",
+        subject: [
+          { name: "笔试", score2: 49 },
+          { name: "机试", score1: 60 },
+          { name: "平均分", score: 54.5 },
+        ],
+      },
+      {
+        dataList: "6.21(星期五)",
+        subject: [
+          { name: "笔试", score2: 42 },
+          { name: "机试", score1: 60 },
+          { name: "平均分", score: 51 },
+        ],
+      },
+    ],
+  },
+  {
+    name: "刘振洲",
+    result: [
+      {
+        dataList: "6.2(星期五)",
+        subject: [
+          { name: "笔试", score2: 55 },
+          { name: "机试", score1: 95 },
+          { name: "平均分", score: 77 },
+        ],
+      },
+      {
+        dataList: "6.9(星期五)",
+        subject: [
+          { name: "笔试", score2: 41 },
+          { name: "机试", score1: 90 },
+          { name: "平均分", score: 65.5 },
+        ],
+      },
+      {
+        dataList: "6.16(星期五)",
+        subject: [
+          { name: "笔试", score2: 48 },
+          { name: "机试", score1: 80 },
+          { name: "平均分", score: 64 },
+        ],
+      },
+      {
+        dataList: "6.21(星期五)",
+        subject: [
+          { name: "笔试", score2: 51 },
+          { name: "机试", score1: 80 },
+          { name: "平均分", score: 65.5 },
+        ],
+      },
+    ],
+  },
+  {
+    name: "杨宏承",
+    result: [
+      {
+        dataList: "6.2(星期五)",
+        subject: [
+          { name: "笔试", score2: 24.5 },
+          { name: "机试", score1: 70 },
+          { name: "平均分", score: 47.25 },
+        ],
+      },
+      {
+        dataList: "6.9(星期五)",
+        subject: [
+          { name: "笔试", score2: 36 },
+          { name: "机试", score1: 60 },
+          { name: "平均分", score: 48 },
+        ],
+      },
+      {
+        dataList: "6.16(星期五)",
+        subject: [
+          { name: "笔试", score2: 38 },
+          { name: "机试", score1: 70 },
+          { name: "平均分", score: 54 },
+        ],
+      },
+      {
+        dataList: "6.21(星期五)",
+        subject: [
+          { name: "笔试", score2: 37 },
+          { name: "机试", score1: 65 },
+          { name: "平均分", score: 51 },
+        ],
+      },
+    ],
+  },
+];
