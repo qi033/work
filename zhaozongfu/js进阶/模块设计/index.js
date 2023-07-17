f2ccd193ce53c9fada5a7896d7d50df3c275236c
zzf <3556526173@qq.com>
2023-07-17 10:51:03 +0800

    提交

---
+++ zhaozongfu/js进阶/模块设计/index.js
@@ -0,0 +1,12 @@
+/* export let index = {
+    name: "明天会更好"
+  }; */
+export const site = "好前程";
+export const func = function () {
+  return "is a module function";
+};
+export class User {
+  show() {
+    console.log("user.show");
+  }
+}
