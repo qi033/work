5932bff4a3620f2c5316b78c1b14dd6ec6409c1e
zzf <3556526173@qq.com>
2023-07-17 20:09:55 +0800

    练习提交

--- zhaozongfu/webpack/src/index.js
+++ zhaozongfu/webpack/src/index.js
@@ -1,4 +1,4 @@
 import $ from "jquery";
 $(function () {
-    $("li:even").css("background", "green");
+    $("li:even").css("backgroundImage", "linear-gradient(0deg,#f43b47 0%, #453a94 100%)");
 });
